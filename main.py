e62d707e9ea6f16c31b7882f7fa73d4f4d533dc6
tian1wang <1327061174@qq.com>
2021-12-07 18:26:13 +0800

    second commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import *
 from Resource import tracepage
 
 # Author: Wang tianyi
+# github: https://github.com/tian1wang
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
